f2f99b026f3a86f92b18742d7c04139e576f0caf
daybreak0 <36679905@qq.com>
2021-01-10 00:43:02 +0800

    UI设计更新

---
+++ BiBi/UIDesign/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UIDesign
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window//继承Windows类
+    {
+        public MainWindow()
+        {
+            InitializeComponent();//加载各种组件
+
+        }
+
+        private void Call_Click(object sender, RoutedEventArgs e)
+        {
+            if(text3.Text=="")
+            {
+                text3.Text = "Calling";
+            }
+            else text3.Text = "";
+        }
+
+        private void Call1_Click(object sender, RoutedEventArgs e)
+        {
+            if (text1.Text == "")
+            {
+                text1.Text = "Calling";
+            }
+            else text1.Text = "";
+        }
+
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            string content = textBox2.Text;
+
+
+        }
+    }
+}
