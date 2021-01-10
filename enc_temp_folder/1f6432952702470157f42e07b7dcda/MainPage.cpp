48347d27c00b8d0abccd9785d4346364160a5b54
王昕凯 <1284776234@qq.com>
2021-01-10 20:05:37 +0800

    美化

--- enc_temp_folder/1f6432952702470157f42e07b7dcda/MainPage.cpp
+++ enc_temp_folder/1f6432952702470157f42e07b7dcda/MainPage.cpp
@@ -53,8 +53,6 @@ namespace winrt::BiBi::implementation
 		// 测试： 载入聊天记录
 		//LoadHistory(L"");
 	}
-	
-	
 	void MainPage::UpdateUserData() {
 		//先置离线
 		for (int i = 0; i < UserDataVM().UserList().Size(); i++) {
@@ -63,26 +61,21 @@ namespace winrt::BiBi::implementation
 		//有回应再置在线
 		FindPeer();
 	}
-	
-	
 	BiBi::TalkMessageViewModel MainPage::TalkMessageVM()
 	{
 		return m_talkMessageVM;
 	}
 
-	
 	BiBi::UserDataViewModel MainPage::UserDataVM()
 	{
 		return m_userDataVM;
 	}
 
-	
 	winrt::hstring MainPage::GetUID()
 	{
 		return m_uId;
 	}
 
-	
 	void MainPage::SetUID(const winrt::hstring& UID)
 	{
 		// 不允许后续更改UID
@@ -92,19 +85,16 @@ namespace winrt::BiBi::implementation
 
 	}
 
-	
 	winrt::hstring MainPage::GetUsername()
 	{
 		return m_username;
 	}
 
-	
 	void MainPage::SetUsername(winrt::hstring const& value)
 	{
 		m_username = value;
 	}
 
-	
 	// 发送消息
 	Windows::Foundation::IAsyncAction MainPage::SendMessage(winrt::hstring hostname,winrt::hstring content)
 	{
@@ -135,8 +125,6 @@ namespace winrt::BiBi::implementation
 		co_await mb.SendToStream(out, Protocol::MessageType::GroupMessageSend, content);
 	}
 	
-
-
 	Windows::Foundation::IAsyncAction MainPage::GroupInvite(winrt::hstring const& hostname, winrt::hstring const& groupName, std::vector<winrt::hstring> const& users) {
 		Windows::Networking::HostName host{ hostname };
 		auto out = co_await WorkerClient.GetTargetStream(host, Port);
@@ -255,7 +243,6 @@ namespace winrt::BiBi::implementation
 		co_await Windows::Storage::FileIO::WriteTextAsync(f, talk_history);
 	}
 
-
 	Windows::Foundation::IAsyncAction MainPage::DeviceInitOrRead()
 	{
 		// 应用数据目录
@@ -296,6 +283,7 @@ namespace winrt::BiBi::implementation
 	}
 
 
+
 	void MainPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
 	{
 		// 启动Udp客户端
@@ -314,7 +302,6 @@ namespace winrt::BiBi::implementation
 		co_await mb.SendToStream(out, Protocol::MessageType::Online, Protocol::Kinds::PeerSeeking);
 	}
 	
-
 	// 页面卸载，保存数据
 	void MainPage::OnNavigatingFrom(Windows::UI::Xaml::Navigation::NavigatingCancelEventArgs const& args) {
 		//保存当前打开的窗口				其他窗口在切换时已经保存
@@ -325,13 +312,11 @@ namespace winrt::BiBi::implementation
 
 	}
 
-
 	void MainPage::OnNavigatedTo(Windows::UI::Xaml::Navigation::NavigationEventArgs const& args)
 	{
 		OutputDebugString(L"Page Loaded\n");
 	}
 	
-
 	winrt::Windows::Foundation::IAsyncAction MainPage::MessageReceived(Windows::Networking::Sockets::DatagramSocket const&, Windows::Networking::Sockets::DatagramSocketMessageReceivedEventArgs const& args)
 	{
 		auto uid = GetUID(), uname = GetUsername();
@@ -403,6 +388,7 @@ namespace winrt::BiBi::implementation
 		//OutputDebugString(L"\n");
 	}
 }
+
 
 void winrt::BiBi::implementation::MainPage::Send_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::RoutedEventArgs const& e)
 {
